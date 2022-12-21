996b1a109634c040edd335117c06aa1721ab1fd1
ismadiandamara <ismadiandamara18@gmail.com>
2022-12-21 23:35:40 +0700

    note_app

---
+++ src/pages/Notes/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useLayoutEffect } from 'react'
+import { SafeAreaView, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import Style from './style';
+import Save from '../../components/saveNote';
+import Delete from '../../components/delNote';
+import ModalNotification from '../../components/Notification';
+
+export default function Notes({route,navigation}){
+    const [date, setDate] = useState(new Date())
+    const [note,setNote] = useState({
+        title:'',
+        note:'',
+        date: date,
+        notificationId: null
+    });
+    const [modalVisible, setModalVisible] = useState(false);
+
+    useEffect(()=>{
+        if(route.params.note){
+            setNote(route.params.note);
+        }
+    },[])
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerRight: () => {
+                return(
+                    <View style={{width: 150, flexDirection:'row', justifyContent: 'space-between', marginRight: 30}}>
+                        <TouchableOpacity onPress={()=>Save(note, navigation)}>
+                            <Feather name="save" size={24} color="black"/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>setModalVisible(!modalVisible)}>
+                            <Feather name="bell" size={24} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>Delete(note, navigation)}>
+                            <Feather name="trash-2" size={24} color="black"/>
+                        </TouchableOpacity>
+                    </View>
+                )
+            }  
+        })
+    },[navigation,note])
+
+    return(
+        <SafeAreaView style={Style.conteiner}>
+            <TextInput 
+                style={Style.txtTitleNote} 
+                autoFocus={true} 
+                maxLength={40}
+                value={note.title} 
+                placeholder={'Judul'}
+                onChangeText={text=>setNote({ ...note, title: text })}
+            >
+            </TextInput>
+            <ScrollView>
+                <TextInput style={Style.txtInput} 
+                    multiline={true} 
+                    value={note.note}
+                    placeholder={'Catatan'}
+                    onChangeText={text=>setNote({ ...note, note: text })}
+                    >
+                </TextInput>
+            </ScrollView>
+            <ModalNotification modalVisible={modalVisible} setModalVisible={setModalVisible} date={date} setDate={setDate} note={note} setNote={setNote}/>
+        </SafeAreaView>
+    )
+}
