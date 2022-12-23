66fa43bdfd4ef0642db236e16226171c0aae83a4
IsmaDDamara <93784246+IsmaDDamara@users.noreply.github.com>
2022-12-23 11:32:17 +0700

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import Home from '../pages/Home'
 import Notes from '../pages/Notes'
+import Login from '../pages/Login'
 import Colors from '../styles/colors'
 
 const Stack = createStackNavigator();
 
-export default function Routes(){
+export default function Routes({Navigation}){
     return(
         <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
             <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
             <Stack.Screen name="Notes" component={Notes} options={{
                 title:'Catatan', 
